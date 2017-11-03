92f980859f9c5d880b303a2ad915327ee9d56f86
Mateusz Bielak <mt.bielak@gmail.com>
2017-11-03 17:35:31 +0100

    All components are now diversified to single files

--- src/app.js
+++ src/app.js
@@ -1,72 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter, Link, NavLink, Route, Switch } from 'react-router-dom';
 import '../node_modules/normalize.css/normalize.css';
 import './styles/styles.scss';
+import AppRouter from './routers/AppRouter.js';
 
-const ExpenseDashboardPage = () => (
-  <div>
-    This is from my dashboard component
-  </div>
-);
-
-const AddExpensePage = () => (
-  <div>
-    This is from my add expense component
-  </div>
-);
-
-const EditExpensePage = () => (
-  <div>
-    This is from my edit expense component
-  </div>
-);
-
-const HelpPage = () => (
-  <div>
-    This is from my help component
-  </div>
-);
-
-const NotFoundPage = () => (
-  <div>
-    404! - <Link to="/" >Go home</Link>
-  </div>
-);
-
-const Header = () => (
-  <header>
-    <h1>Expensify</h1>
-    <NavLink to="/" activeClassName='is-active' exact={true} >Dashboard</NavLink>
-    <NavLink to="/create" activeClassName='is-active'>Create Expense</NavLink>
-    <NavLink to="/edit" activeClassName='is-active'>Edit Expense</NavLink>
-    <NavLink to="/help" activeClassName='is-active'>Help</NavLink>
-  </header>
-);
-
-const routes = (
-<BrowserRouter>
-  <div>
-    <Header />
-    <Switch>
-      <Route
-        path='/'
-        component={ ExpenseDashboardPage }
-        exact={ true } />
-      <Route
-        path='/create'
-        component={ AddExpensePage } />
-      <Route
-        path='/edit'
-        component={ EditExpensePage } />
-      <Route
-        path='/help'
-        component={ HelpPage } />
-      <Route component={ NotFoundPage } />
-    </Switch>
-  </div>
-  
-</BrowserRouter>
-);
-
-ReactDOM.render(routes, document.querySelector("#app"));
+ReactDOM.render(<AppRouter />, document.querySelector("#app"));
---
+++ src/routers/AppRouter.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import { BrowserRouter, Link, NavLink, Route, Switch } from 'react-router-dom';
+import ExpenseDashboardPage from '../components/ExpenseDashboardPage.js';
+import AddExpensePage from '../components/AddExpensePage.js';
+import EditExpensePage from '../components/EditExpensePage.js';
+import HelpPage from '../components/HelpPage.js';
+import NotFoundPage from '../components/NotFoundPage.js';
+import Header from '../components/Header.js';
+
+const AppRouter = () => (
+  <BrowserRouter>
+    <div>
+      <Header />
+      <Switch>
+        <Route
+          path='/'
+          component={ ExpenseDashboardPage }
+          exact={ true } />
+        <Route
+          path='/create'
+          component={ AddExpensePage } />
+        <Route
+          path='/edit'
+          component={ EditExpensePage } />
+        <Route
+          path='/help'
+          component={ HelpPage } />
+        <Route component={ NotFoundPage } />
+      </Switch>
+    </div>
+  </BrowserRouter>
+);
+
+export default AppRouter;
