7a3c272d5abed4d1cee42ce34cba7620d1b60b43
Mateusz Bielak <mt.bielak@gmail.com>
2017-11-08 09:32:25 +0100

    JS Lint refactoring

--- webpack.config.js
+++ webpack.config.js
@@ -4,25 +4,25 @@ module.exports = {
   entry: './src/app.js',
   output: {
     path: path.join(__dirname, 'public'),
-    filename: 'bundle.js'
+    filename: 'bundle.js',
   },
   module: {
     rules: [{
       loader: 'babel-loader',
       test: /\.js$/,
-      exclude: /(node_modules|public)/
+      exclude: /(node_modules|public)/,
     }, {
       test: /\.s?css$/,
       use: [
         'style-loader',
         'css-loader',
-        'sass-loader'
-      ]
-    }]
+        'sass-loader',
+      ],
+    }],
   },
   devtool: 'cheap-module-eval-source-map',
   devServer: {
     contentBase: path.join(__dirname, 'public'),
-    historyApiFallback: true
-  }
+    historyApiFallback: true,
+  },
 };
