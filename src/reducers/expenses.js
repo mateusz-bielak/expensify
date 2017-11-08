a25d09320ce18a95bf62e267b21bccecb07a08d2
Mateusz Bielak <mt.bielak@gmail.com>
2017-11-08 09:31:47 +0100

    Redux reducers

---
+++ src/reducers/expenses.js
@@ -0,0 +1,25 @@
+const expensesReducerDefalutState = [];
+
+const expensesReducer = (state = expensesReducerDefalutState, action) => {
+  switch (action.type) {
+    case 'ADD_EXPENSE': {
+      return [...state, action.expense];
+    } case 'REMOVE_EXPENSE': {
+      return state.filter(expense => expense.id !== action.id);
+    } case 'EDIT_EXPENSE': {
+      return state.map((expense) => {
+        if (expense.id === action.id) {
+          return {
+            ...expense,
+            ...action.updates,
+          };
+        }
+        return expense;
+      });
+    } default: {
+      return state;
+    }
+  }
+};
+
+export default expensesReducer;
