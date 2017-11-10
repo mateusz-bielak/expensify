c3e1dd98753837f8893f4f3777ed46f0a9f7f4da
Mateusz Bielak <mt.bielak@gmail.com>
2017-11-11 00:29:19 +0100

    Setting redux devtools

--- src/store/configureStore.js
+++ src/store/configureStore.js
@@ -2,10 +2,15 @@ import { createStore, combineReducers } from 'redux';
 import expensesReducer from '../reducers/expenses';
 import filtersReducer from '../reducers/filters';
 
+/* eslint-disable no-underscore-dangle */
 export default () => {
-  const store = createStore(combineReducers({
-    expenses: expensesReducer,
-    filters: filtersReducer,
-  }));
+  const store = createStore(
+    combineReducers({
+      expenses: expensesReducer,
+      filters: filtersReducer,
+    }),
+    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(),
+  );
+  /* eslint-enable */
   return store;
 };
