9649c9835b37b5bac724c72a02eee24a0092bef4
Mateusz Bielak <mt.bielak@gmail.com>
2017-11-11 15:14:19 +0100

    File can be see by test command

--- src/tests/actions/expenses.test.js
+++ src/tests/actions/expenses.test.js
@@ -1,24 +1,5 @@
 import { addExpense, editExpense, removeExpense } from '../../actions/expenses';
 
-test('should setup remove expense action object', () => {
-  const action = removeExpense({ id: '123abc' });
-  expect(action).toEqual({
-    type: 'REMOVE_EXPENSE',
-    id: '123abc',
-  });
-});
-
-test('should setup edit expense action object', () => {
-  const action = editExpense('123abc', { note: 'New note value' });
-  expect(action).toEqual({
-    type: 'EDIT_EXPENSE',
-    id: '123abc',
-    updates: {
-      note: 'New note value',
-    },
-  });
-});
-
 test('should setup add expense action object with provided values', () => {
   const expenseData = {
     description: 'Testing description',
@@ -47,5 +28,24 @@ test('should setup add expense action object with default values', () => {
       createdAt: 0,
       id: expect.any(String),
     },
+  });
+});
+
+test('should setup edit expense action object', () => {
+  const action = editExpense('123abc', { note: 'New note value' });
+  expect(action).toEqual({
+    type: 'EDIT_EXPENSE',
+    id: '123abc',
+    updates: {
+      note: 'New note value',
+    },
+  });
+});
+
+test('should setup remove expense action object', () => {
+  const action = removeExpense({ id: '123abc' });
+  expect(action).toEqual({
+    type: 'REMOVE_EXPENSE',
+    id: '123abc',
   });
 });
