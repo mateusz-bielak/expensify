000d838b40c2dfc7306f832ac3661417887aae6d
Mateusz Bielak <mt.bielak@gmail.com>
2017-11-15 16:09:38 +0100

    Resolved PropTypes bug

--- src/tests/fixtures/expenses.js
+++ src/tests/fixtures/expenses.js
@@ -5,19 +5,19 @@ const expenses = [{
   description: 'Car',
   note: '',
   amount: 300000,
-  createdAt: moment(0),
+  createdAt: moment(0).valueOf(),
 }, {
   id: '2',
   description: 'Vacation',
   note: '',
   amount: 50000,
-  createdAt: moment(0).add(4, 'days'),
+  createdAt: moment(0).add(4, 'days').valueOf(),
 }, {
   id: '3',
   description: 'Dog',
   note: '',
   amount: 2500,
-  createdAt: moment(0).subtract(4, 'days'),
+  createdAt: moment(0).subtract(4, 'days').valueOf(),
 }];
 
 export default expenses;
