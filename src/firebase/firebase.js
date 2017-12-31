48fcc7a698c64e015103a392722cae30611ad2f4
Mateusz Bielak <mt.bielak@gmail.com>
2017-12-31 13:53:07 +0100

    Seperate Database setup for testing purposes

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -1,12 +1,12 @@
 import * as firebase from 'firebase';
 
 const config = {
-  apiKey: 'AIzaSyBt6vetvQMwG9KuUf99Rgt-pADNd9tWLsg',
-  authDomain: 'expensify-9313.firebaseapp.com',
-  databaseURL: 'https://expensify-9313.firebaseio.com',
-  projectId: 'expensify-9313',
-  storageBucket: 'expensify-9313.appspot.com',
-  messagingSenderId: '774138614776',
+  apiKey: process.env.FIREBASE_API_KEY,
+  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+  databaseURL: process.env.FIREBASE_DATABASE_URL,
+  projectId: process.env.FIREBASE_PROJECT_ID,
+  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
 };
 
 firebase.initializeApp(config);
