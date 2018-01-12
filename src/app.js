d4dad90c1ea3dc1227305ebceaab9259162fad92
Mateusz Bielak <mt.bielak@gmail.com>
2018-01-12 20:43:34 +0100

    Added auth reducer for managing user id in the state

--- src/app.js
+++ src/app.js
@@ -7,6 +7,7 @@ import 'react-dates/lib/css/_datepicker.css';
 import AppRouter, { history } from './routers/AppRouter';
 import configureStore from './store/configureStore';
 import { startSetExpenses } from './actions/expenses';
+import { login, logout } from './actions/auth';
 import '../node_modules/normalize.css/normalize.css';
 import './styles/styles.scss';
 import { firebase } from './firebase/firebase';
@@ -31,6 +32,7 @@ ReactDOM.render(<p>Loading...</p>, document.querySelector('#app'));
 
 firebase.auth().onAuthStateChanged((user) => {
   if (user) {
+    store.dispatch(login(user.uid));
     store.dispatch(startSetExpenses()).then(() => {
       renderApp();
       if (history.location.pathname === '/') {
@@ -38,6 +40,7 @@ firebase.auth().onAuthStateChanged((user) => {
       }
     });
   } else {
+    store.dispatch(logout());
     renderApp();
     history.push('/');
   }
--- src/store/configureStore.js
+++ src/store/configureStore.js
@@ -1,5 +1,6 @@
 import { createStore, combineReducers, applyMiddleware, compose } from 'redux';
 import thunk from 'redux-thunk';
+import authReducer from '../reducers/auth';
 import expensesReducer from '../reducers/expenses';
 import filtersReducer from '../reducers/filters';
 
@@ -10,6 +11,7 @@ const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 export default () => {
   const store = createStore(
     combineReducers({
+      auth: authReducer,
       expenses: expensesReducer,
       filters: filtersReducer,
     }),
