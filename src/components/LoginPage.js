d70405e81471876c5f3ed8a260d026660d6c1c24
Mateusz Bielak <mt.bielak@gmail.com>
2018-01-17 03:50:47 +0100

    Login Page button and Name styling

--- src/components/LoginPage.js
+++ src/components/LoginPage.js
@@ -6,9 +6,9 @@ import { startLogin } from '../actions/auth';
 export const LoginPage = props => (
   <div className="box-layout">
     <div className="box-layout__box">
-      <h1 className="box-layout__title">Expensify App</h1>
+      <h1 className="box-layout__title">Expensify</h1>
       <p>It&apos;s time to get your expenses under control.</p>
-      <button onClick={props.startLogin} >Login</button>
+      <button className="button" onClick={props.startLogin}>Login with Google</button>
     </div>
   </div>
 );
