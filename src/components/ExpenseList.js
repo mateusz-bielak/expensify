d652fbbe63fdddab92430a288af74b8ec309efa1
Mateusz Bielak <mt.bielak@gmail.com>
2018-01-19 12:12:14 +0100

    Expense List container & header styling

--- src/components/ExpenseList.js
+++ src/components/ExpenseList.js
@@ -6,8 +6,12 @@ import getVisibleExpenses from '../selectors/expenses';
 
 // Exporting for testing purposes
 export const ExpenseList = props => (
-  <div>
-    <h1>Expense List</h1>
+  <div className="content-container">
+    <div className="list-header">
+      <div className="show-for-mobile">Expenses</div>
+      <div className="show-for-desktop">Expense</div>
+      <div className="show-for-desktop">Amount</div>
+    </div>
     {props.expenses.length !== 0 ? (
       props.expenses.map(expense => <ExpenseListItem key={expense.id} {...expense} />)
     ) : (
